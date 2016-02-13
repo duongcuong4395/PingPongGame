5262b3d30a5f93a5310ad80210497f856843f6c6
duongcuong <dangduongcuong@gmail.com>
2016-02-13 10:34:36 +0700

    - Add library\n- Add .gitignore

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public Vector3 initialImpulse;
+
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().AddForce (initialImpulse, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
